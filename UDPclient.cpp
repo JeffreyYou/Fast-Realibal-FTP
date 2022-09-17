165ecbfc482d109a28998c3fb0b8a0fb2c7acf4f
RunhaiLin <47149190+RunhaiLin@users.noreply.github.com>
2022-09-18 04:40:52 +0800

    Add files via upload

--- UDPclient.cpp
+++ UDPclient.cpp
@@ -17,18 +17,49 @@
 #include <algorithm>
 #include <map>
 #include <fstream>
+
+#include <thread>
+#include <chrono>
+#include <mutex>
 using namespace std;
 
-void FileHandler(string s,ofstream &out){
-    if(out.is_open()){
-            out << s <<endl;
-            out.flush();
-        }else{
-            cout << "Not opening" <<endl;
+
+static int currenttoken;
+static ofstream out;
+mutex mtx;
+
+void FileHandler(string s,int token,int sck,sockaddr_in hintM){
+    cout << "Hello " << token << endl;
+    while(1){
+        mtx.lock();
+        string t = to_string(currenttoken);
+        if(token > currenttoken){
+            int sendMain = sendto(sck,t.c_str(),t.size()+1,0,(sockaddr*)&hintM,sizeof(hintM));
+            mtx.unlock();
+            usleep(200);
+        } else if (token < currenttoken){   
+            //drop 
+            mtx.unlock();
+            break;
+        } else{
+            currenttoken++;
+            if(out.is_open()){
+                out << s <<endl;
+                out.flush();
+            }else{
+                cout << "Not opening" <<endl;
+            }
+            mtx.unlock();
+            break;
+        }
     }
+
+   
 }
 
 int main(){
+    currenttoken = 0;
+
     //create a socket
     int port = 31533;
     int sck = socket(AF_INET, SOCK_DGRAM, 0);
@@ -36,6 +67,12 @@ int main(){
     hint.sin_family = AF_INET;
     hint.sin_port = htons(port);
     hint.sin_addr.s_addr = INADDR_ANY;
+
+    struct sockaddr_in hintM;
+    hintM.sin_family = AF_INET;
+    hintM.sin_port = htons(30533);
+    inet_pton(AF_INET,"127.0.0.1",&hintM.sin_addr);
+
     int test1 = ::bind(sck,(sockaddr*)&hint,sizeof(hint));
     if (test1 == -1){
         cout << "test1" << "\n";
@@ -44,17 +81,24 @@ int main(){
 
     struct sockaddr_in client;
     socklen_t len = sizeof(client);
-    ofstream out;
     out.open("b.txt", ios_base::app);
-    
+
     char buf[66000]; 
 
     while(true){
         memset(buf, 0, 66000);
         int bin = recvfrom(sck,buf,1024,0,(struct sockaddr*) &client,&len);
         string s(buf);
-        
-        FileHandler(s,out);   
+
+        // token number ???
+        char t = s[0];
+        int token = t - '0'; 
+        cout << "Token " << token <<endl;
+        thread client_thread(FileHandler,s,token,sck,hintM);
+
+        // thread client_thread(FileHandler,out);
+        // client_thread.join();
+         
     }
 
     out.close();
