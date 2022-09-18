67a921b3cada44c295f2c734dfa4d2ac61030895
JeffreyYou <yqse521749@gmail.com>
2022-09-17 22:52:11 -0700

    update

--- ftp_Client.cpp
+++ ftp_Client.cpp
@@ -8,24 +8,146 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
-
+#include <map>
 #include <iostream>
+#include <thread>
 #include <fstream>
+#include <mutex>
+
 using namespace std;
 
+int server_sockfd;
 #define SERVER_PORT 9999
-#define CLIENT_PORT 8888
+map<int,string> packet_map;
+int current_token=0;
+
+char recv_buffer[2000];
+char file_name[100];
+char packet_num[100];
+char write_buffer[2000];
+
+bool isFinish = false;
+int total_sotred_packet=0;
+int total_packetNum = 0;
+int tok_len = 6;
+
+bool* check_p;  
+struct sockaddr_in client_recv_address, client_send_address;
+
+
+mutex mtx;
+void set_token(int token_, char* thistok){
+        //convert token to char array of 00xxxx format
+        string count_str;
+        count_str = to_string(token_);
+        tok_len = tok_len-count_str.length();
+        while(tok_len != 0){
+            count_str = "0" + count_str;
+            tok_len = tok_len - 1;
+        }
+        tok_len = 6;
+        strcpy(thistok, count_str.c_str());
+}
+int resend_packet(int number){
+        char resend_buffer[6];
+        set_token(number, resend_buffer);
+        mtx.lock();
+        sendto(server_sockfd, resend_buffer, sizeof(resend_buffer), 0, (struct sockaddr*)&client_send_address, sizeof(client_send_address));
+        mtx.unlock();
+
+        return 0;
+}
+
+int store_packet_in_map(){
+        int before=0;
+        int now=0;  
+
+        while(total_sotred_packet!=total_packetNum){
+
+            memset(recv_buffer, 0, sizeof(recv_buffer));
+            int numbytes = recvfrom(server_sockfd, recv_buffer, 2000, 0, NULL, 0);
+            int token_num = 0;
+            for(int j =0;j<6;j++){
+                token_num = token_num * 10 + (recv_buffer[j]-'0');
+            }
+            cout<<"store packet: "<<token_num<<endl;
+            now = token_num;
+            if((now-before)>1){ //some packet lost
+                //resend before+1
+
+                thread requset_packet(resend_packet,before+1);
+                //usleep();
+
+            }
+            if(check_p[token_num] == false){ //not flase means drop this packet
+                mtx.lock();
+                check_p[token_num] == true;
+                string str(recv_buffer);
+                pair<int,string> p(token_num, str);
+                packet_map.insert(p);
+                total_sotred_packet++;
+                before = token_num;
+                mtx.unlock();
+            }
+
+        }
+        return 0;
+}
+int write_packet_func(){
+        ofstream write_file;
+        write_file.open("./data_client.txt");
+
+        while(current_token != total_packetNum){
+            if(packet_map.count(current_token)>0){
+                memset(write_buffer, 0, sizeof(write_buffer));
+
+                //write_buffer = packet_map[current_token].c_str();
+                write_file<< packet_map[current_token].substr(6);
+                mtx.lock();
+                packet_map.erase(current_token);
+                mtx.unlock();
+                current_token++;
+            }else{
+                thread requset_packet(resend_packet,current_token);
+
+                //resend
+            }
+        }
+        return 0;
+}
+   
+
+    // int i = 27;
+    // ofstream write_file;
+    // write_file.open("./data_client.txt");
+    // int token_num;
+    // while(i>0){
+    //     numbytes = recvfrom(server_sockfd, recv_buffer, 2000, 0, NULL, 0);
+    //     token_num = 0;
+    //     for(int j =0;j<6;j++){
+    //         token_num = token_num * 10 + (recv_buffer[j]-'0');
+    //     }
+
+    //     cout<< "Receive packet: "<<token_num <<endl;
+    //     write_file<< recv_buffer+6;
+    //     memset(recv_buffer, 0, sizeof(recv_buffer));
+    //     i--;
+    // }
+    // write_file.close();
 
 int main(int argc, char const *argv[]){
 
     char send_buffer[] = "get ./data.txt";
-    char recv_buffer[2000];
+    
+
+    string fileName;
+    int packetNum;
+
     memset(recv_buffer, 0, sizeof(recv_buffer));
 
-    struct sockaddr_in client_recv_address, client_send_address;
 
     //create socket to send message to server
-    int server_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    server_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     bzero(&client_send_address, sizeof(client_send_address));
 
     client_send_address.sin_family = AF_INET;
@@ -40,39 +162,36 @@ int main(int argc, char const *argv[]){
     }
 
     //recv file name
-    numbytes = recvfrom(server_sockfd, recv_buffer, 100, 0, NULL, 0);
-    cout<<"the name of the file: \""<< recv_buffer <<"\""<< endl;
+    memset(file_name, 0, sizeof(file_name));
+    numbytes = recvfrom(server_sockfd, file_name, 100, 0, NULL, 0);
+    //store the file name
+    fileName = file_name; 
+    cout<<"the name of the file: \""<< fileName <<"\""<< endl;
     //create file
     ofstream create_file;
     create_file.open("data_client.txt", ios::out | ios::trunc);
     create_file.close();
     
-    memset(recv_buffer, 0, sizeof(recv_buffer));
-    numbytes = recvfrom(server_sockfd, recv_buffer, 100, 0, NULL, 0);
-    cout<<"the total packet to be sent: \""<< recv_buffer <<"\""<< endl;
-    memset(recv_buffer, 0, sizeof(recv_buffer));
-
-
-    int i = 27;
-    ofstream write_file;
-    write_file.open("./data_client.txt");
-    int token_num;
-    while(i>0){
-        numbytes = recvfrom(server_sockfd, recv_buffer, 2000, 0, NULL, 0);
-        token_num = 0;
-        for(int j =0;j<6;j++){
-            token_num = token_num * 10 + (recv_buffer[j]-'0');
-        }
-
-        cout<< "Receive packet: "<<token_num <<endl;
-        write_file<< recv_buffer+6;
-        memset(recv_buffer, 0, sizeof(recv_buffer));
-        i--;
+    memset(packet_num, 0, sizeof(packet_num));
+    numbytes = recvfrom(server_sockfd, packet_num, 100, 0, NULL, 0);
+    //store the packet number
+    for(int s=0; s<strlen(packet_num); s++){
+        total_packetNum = total_packetNum*10 + (packet_num[s]-'0');
     }
-    write_file.close();
-    //cout<<"Server starts sending file \""<< recv_buffer <<"\""<< endl;
-    close(server_sockfd);
+    cout<<"the total packet to be sent: \""<< total_packetNum <<"\""<< endl;
 
+    bool recv_check[packetNum];
+    for(int i = 0;i<packetNum;i++){
+        recv_check[i] = false;
+    }
+    check_p = recv_check;
+
+    thread store_packet(store_packet_in_map);
+    thread write_packet(write_packet_func);
+
+    store_packet.join();
+    write_packet.join();
+    close(server_sockfd);
     return 0;
 
 }
